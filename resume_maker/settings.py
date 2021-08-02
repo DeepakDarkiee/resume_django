76e54ca0be8e11c7742a93300d82ad51ff441262
ishwar <ishwar.thoughtwin@gmail.com>
2021-08-02 11:29:58 +0530

    .

--- resume_maker/settings.py
+++ resume_maker/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'resume_maker.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
