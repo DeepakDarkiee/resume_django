e20e7c6791200bd60c223eeff20f4171a5c01fe2
ishwar <ishwar.thoughtwin@gmail.com>
2021-08-02 12:35:13 +0530

    .

--- resume_maker/settings.py
+++ resume_maker/settings.py
@@ -92,6 +92,8 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True
 
 
 
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
