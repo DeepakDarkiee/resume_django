d52f0279ecded1ddf974f6ef9b4976784432ec3a
ishwar <ishwar.thoughtwin@gmail.com>
2021-08-04 11:38:34 +0530

    add_media_file

--- resume_maker/settings.py
+++ resume_maker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
 import os
 from pathlib import Path
 # import django_heroku
@@ -54,7 +55,6 @@ MIDDLEWARE = [
 
 
 ]
-
 
 
 ROOT_URLCONF = 'resume_maker.urls'
